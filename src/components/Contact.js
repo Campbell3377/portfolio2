0286eb929ba90aca245128eb9a4e86abbe5ca1ff
Sean Campbell <seancampbell3377@gmail.com>
2023-05-20 10:30:48 -0700

    fixed Contact

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -23,7 +23,7 @@ export default function Contact() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
+      body: encode({ "form-name": "contact", name, email, message}),
     })
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
